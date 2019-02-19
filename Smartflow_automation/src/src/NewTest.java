a5df9b4deea3b4ea222d35cb99eaa0c5ebc7452d
hemangini9391 <hemangini@risingappsense.com>
2019-02-19 15:17:41 +0530

    commit
    commit

--- Smartflow_automation/src/src/NewTest.java
+++ Smartflow_automation/src/src/NewTest.java
@@ -1,10 +1,10 @@
 package src;
 
+import org.testng.annotations.Test;
 import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.Test;
 import java.awt.Toolkit;
 import java.awt.AWTException;	
 import java.awt.Robot;
@@ -27,54 +27,49 @@ public class NewTest {
 	    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);   
     }
 	
-	@Test(priority=1)
+	@Test(priority=2)
+	
     public void file()throws InterruptedException, AWTException 
 	{
 	 robot = new Robot();  // Robot class throws AWT Exception
      driver.findElement(By.id("imagesrc")).click();
-	 robot.setAutoDelay(1000);
+	 robot.setAutoDelay(2000);
 	 
 	//Store the location of the file in clipboard 
-    //Clipboard
+	//Clipboard
+		 
+    strSel = new StringSelection("‪This PC");
+	Toolkit.getDefaultToolkit().getSystemClipboard().setContents(strSel, null);
+	robot.setAutoDelay(1000);
 	 
-	 strSel = new StringSelection ("‪F:");
-	 Toolkit.getDefaultToolkit().getSystemClipboard().setContents(strSel, null);
-	 
-	 robot.setAutoDelay(1000);
-	 
+	
+	// press cntrol + L and relaese
+	robot.keyPress(KeyEvent.VK_CONTROL);
+	robot.keyPress(KeyEvent.VK_L);
+	robot.keyRelease(KeyEvent.VK_CONTROL);
+	
+	// Backspace
+	
+	robot.keyPress(KeyEvent.VK_BACK_SPACE);
+    robot.keyRelease(KeyEvent.VK_BACK_SPACE);
+	
 	 // press cntrol + v and relaese
-	 
+
 	 robot.keyPress(KeyEvent.VK_CONTROL);
-	 robot.keyPress(KeyEvent.VK_V);
+     robot.keyPress(KeyEvent.VK_V);
 	 robot.keyRelease(KeyEvent.VK_CONTROL);
 	 
 	
 	 // press enter key and relaese 
-	 
-	 
+	  
 	 Thread.sleep(1000);
-//	 robot.keyPress(KeyEent.VK_ENTER);
-//	 robot.keyRelease(KeyEvent.VK_ENTER);
-	 robot.keyPress(KeyEvent.VK_ALT);
-	 robot.keyPress(KeyEvent.VK_O);
-	 robot.keyRelease(KeyEvent.VK_ALT); 
-	 
-	 strSel = new StringSelection ("‪Screenshot_1.png");
-	 Toolkit.getDefaultToolkit().getSystemClipboard().setContents(strSel, null);
-	 
-	 robot.setAutoDelay(1000);
-	 
-	 // press cntrol + v and relaese
-	 robot.keyPress(KeyEvent.VK_CONTROL);
-	 robot.keyPress(KeyEvent.VK_V);
-	 robot.keyRelease(KeyEvent.VK_CONTROL);
-	
-	 // press enter key and relaese 
-	 
-	 Thread.sleep(3000);
 	 robot.keyPress(KeyEvent.VK_ENTER);
-	 robot.keyRelease(KeyEvent.VK_ENTER);
+//   robot.keyRelease(KeyEvent.VK_ENTER);
+//	 robot.keyPress(KeyEvent.VK_ALT);
 	 
+	 
+//	 robot.keyPress(KeyEvent.VK_O);
+//	 robot.keyRelease(KeyEvent.VK_ALT);  
 	 
     }
 }
