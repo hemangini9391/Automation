0b202389de372d300bd90033bf3ea27ef63ce836
hemangini9391 <hemangini@risingappsense.com>
2019-02-28 16:05:19 +0530

    commit
    commit

--- Smartflow_automation/src/src/Csv_upload_test.java
+++ Smartflow_automation/src/src/Csv_upload_test.java
@@ -6,70 +6,65 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import java.awt.Toolkit;
-import java.awt.AWTException;	
+import java.awt.AWTException;
 import java.awt.Robot;
 import java.awt.event.KeyEvent;
 import java.awt.datatransfer.StringSelection;
 
 public class Csv_upload_test {
-	
+
 	Robot robot;
-	WebDriver driver ;
-    StringSelection strSel;
-	
-	@Test(priority=1)
+	WebDriver driver;
+	StringSelection strSel;
 
-    public void Browserlaunch()throws InterruptedException 
-	{
-	    System.setProperty("webdriver.chrome.driver","C:\\Users\\rising1\\Desktop\\chromedriver.exe");
-	    driver = new ChromeDriver(); 
-	    driver.get("http://demo.automationtesting.in/Register.html");
-	    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);   
-    }
-	
-	@Test(priority=2)
-	
-    public void file()throws InterruptedException, AWTException 
-	{
-	 robot = new Robot();  // Robot class throws AWT Exception
-     driver.findElement(By.id("imagesrc")).click();
-	 robot.setAutoDelay(2000);
-	 
-	//Store the location of the file in clipboard 
-	//Clipboard
-		 
-    strSel = new StringSelection("‪This PC");
-	Toolkit.getDefaultToolkit().getSystemClipboard().setContents(strSel, null);
-	robot.setAutoDelay(1000);
-	 
-	
-	// press cntrol + L and relaese
-	robot.keyPress(KeyEvent.VK_CONTROL);
-	robot.keyPress(KeyEvent.VK_L);
-	robot.keyRelease(KeyEvent.VK_CONTROL);
-	
-	// Backspace
-	
-	robot.keyPress(KeyEvent.VK_BACK_SPACE);
-    robot.keyRelease(KeyEvent.VK_BACK_SPACE);
-	
-	 // press cntrol + v and relaese
+	@Test(priority = 1)
 
-	 robot.keyPress(KeyEvent.VK_CONTROL);
-     robot.keyPress(KeyEvent.VK_V);
-	 robot.keyRelease(KeyEvent.VK_CONTROL);
-	 
-	
-	 // press enter key and relaese 
-	  
-	 Thread.sleep(1000);
-	 robot.keyPress(KeyEvent.VK_ENTER);
-//   robot.keyRelease(KeyEvent.VK_ENTER);
-//	 robot.keyPress(KeyEvent.VK_ALT);
-	 
-	 
-//	 robot.keyPress(KeyEvent.VK_O);
-//	 robot.keyRelease(KeyEvent.VK_ALT);  
-	 
-    }
+	public void Browserlaunch() throws InterruptedException {
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\rising1\\Desktop\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.get("http://demo.automationtesting.in/Register.html");
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	}
+
+	@Test(priority = 2)
+
+	public void file() throws InterruptedException, AWTException {
+		robot = new Robot(); // Robot class throws AWT Exception
+		driver.findElement(By.id("imagesrc")).click();
+		robot.setAutoDelay(2000);
+
+		// Store the location of the file in clipboard
+		// Clipboard
+
+		strSel = new StringSelection("‪This PC");
+		Toolkit.getDefaultToolkit().getSystemClipboard().setContents(strSel, null);
+		robot.setAutoDelay(1000);
+
+		// press cntrol + L and relaese
+		robot.keyPress(KeyEvent.VK_CONTROL);
+		robot.keyPress(KeyEvent.VK_L);
+		robot.keyRelease(KeyEvent.VK_CONTROL);
+
+		// Backspace
+
+		robot.keyPress(KeyEvent.VK_BACK_SPACE);
+		robot.keyRelease(KeyEvent.VK_BACK_SPACE);
+
+		// press cntrol + v and relaese
+
+		robot.keyPress(KeyEvent.VK_CONTROL);
+		robot.keyPress(KeyEvent.VK_V);
+		robot.keyRelease(KeyEvent.VK_CONTROL);
+
+		// press enter key and relaese
+
+		Thread.sleep(1000);
+		robot.keyPress(KeyEvent.VK_ENTER);
+		// robot.keyRelease(KeyEvent.VK_ENTER);
+		// robot.keyPress(KeyEvent.VK_ALT);
+
+		// robot.keyPress(KeyEvent.VK_O);
+		// robot.keyRelease(KeyEvent.VK_ALT);
+
+	}
 }
